8e63f1dfb0fd4e42f6a53c67d0d40ca94d665301
Santiago Ospina <santiagoospinarojas27@gmail.com>
2023-01-12 15:12:26 -0500

    feat: update part

--- app/Http/Controllers/Service/SparePartsController.php
+++ app/Http/Controllers/Service/SparePartsController.php
@@ -18,16 +18,39 @@ class SparePartsController {
 	}
 
 	public function readSparePartsById($idspare_parts) {
-        return $this->sparePartsModel->readSparePartsByIdDB($idspare_parts);
-    }
+		return $this->sparePartsModel->readSparePartsByIdDB($idspare_parts);
+	}
 
 	public function createSpareParts() {
-		return $this->sparePartsModel->createSparePartsDB(SpareParts::formFields());
+		$createSpareParts= $this->sparePartsModel->createSparePartsDB(
+			SpareParts::formFields()->setSparePartsDigitalQuantity(request->spare_parts_amount)
+		);
+
+		if($createSpareParts->status === 'database-error') {
+			return response->error('Ocurrió un error al crear el repuesto');
+		}
+
+		return response->success('Repuesto creado correctamente');
 	}
 
 	public function updateSpareParts() {
 
-		return $this->sparePartsModel->updateSparePartsDB(SpareParts::formFields());
+		$parts= $this->sparePartsModel->readSparePartsByIdDB(request->idspare_parts);
+
+		if(request->spare_parts_amount > $parts->spare_parts_digital_quantity) {
+			$spare_parts_amount=request->spare_parts_amount + $parts->spare_parts_amount;
+			$amount_quantity=request->spare_parts_amount + $parts->spare_parts_digital_quantity;
+		}
+
+		$updateSpareParts = $this->sparePartsModel->updateSparePartsDB(
+			SpareParts::formFields()
+			->setSparePartsDigitalQuantity($amount_quantity)
+			->setSparePartsAmount($spare_parts_amount)
+		);
+		if($updateSpareParts->status === 'database-error') {
+			return response->error('Ocurrió un error al actualizar el repuesto');
+		}
+		return response->success('Repuesto Actualizado  correctamente');
 	}
 
 }
