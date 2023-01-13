d4f2c08ec875b34bb29858b912c3779a7f90f3cf
Santiago Ospina <santiagoospinarojas27@gmail.com>
2023-01-13 16:24:49 -0500

    feat: validations were added

--- app/Http/Controllers/Service/TechnicalInventoryController.php
+++ app/Http/Controllers/Service/TechnicalInventoryController.php
@@ -37,33 +37,93 @@ class TechnicalInventoryController {
         return $this->technicalInventoryModel->readTechnicalInventoryDB();
     }
 
+
+
+
     public function updateTechnicalInventory(){
         $sparts = SpareParts::formFields();
         $Technical = TechnicalInventory::formFields();
-        $readSpareParts= $this->sparePartsModel->readBySparePartsDB($sparts->getIdspareParts());
-        $sparts->setSparePartsAmount($readSpareParts->spare_parts_amount);
 
-        if($Technical->getTechnicalInventoryAmount() > $sparts->getSparePartsAmount()) {
-            return response->error("la cantidad ingresada sobre pasa a la que hay en el inventario");
-        }
         if($Technical->getIdserviceStates() == 3) {
-            $sparts->setSparePartsAmount($sparts->getSparePartsAmount() - $Technical->getTechnicalInventoryAmount());
-            $this->sparePartsModel->update_spare_parts_amount($sparts);
-            $responseUpdate = $this->technicalInventoryModel->updateTechnicalInventoryDB($Technical);
-            return response->success("Inventario del tecnico actualizado correctamente 3");
-        }
-        elseif ($Technical->getIdserviceStates() == 8) {
-            if( $Technical->getTechnicalInventoryQuantityUsed() <= $Technical->getTechnicalInventoryAmount()) {
-                $Technical->setTechnicalInventoryAmount(
-                    $Technical->getTechnicalInventoryAmount() - $Technical->getTechnicalInventoryQuantityUsed()
-                );
-                $responseUpdate = $this->technicalInventoryModel->updateReduceTechnicalInventoryDB($Technical);
-                return response->success("Inventario del tecnico actualizado correctamente 8");
-            }
-            else{
-             return response->error("Error la cantidad de ingresada de repuestos usados es mayor a la cantidad de repuesto que tiene en su inventario verifique porfavor");
-            }
-        }
-    }
+           return $readSpareParts= $this->sparePartsModel->readBySparePartsDB($Technical->getIdspareParts());
+           if($sparts->getSparePartsAmount() >= $Technical->getTechnicalInventoryAmount()) {
+               $sparts->setSparePartsAmount($sparts->getSparePartsAmount() - $Technical->getTechnicalInventoryAmount());
+               $this->sparePartsModel->update_spare_parts_amount($sparts);
+               $this->technicalInventoryModel->updateTechnicalInventoryDB($Technical);
+           }
+       }
+       $readTechnical=$this->technicalInventoryModel->readByTechnicalInventoryDB($Technical);
+       if($Technical->getIdserviceStates() == 3){
+           $readTechnical=$this->technicalInventoryModel->readByTechnicalInventoryDB($Technical);
+           if(isset($readTechnical->idusers)) {
+              $Technical->setIdtechnicalInventory($readTechnical->idtechnical_inventory);
+              $Technical->setTechnicalInventoryAmount(
+                  $readTechnical->technical_inventory_amount + $Technical->getTechnicalInventoryAmount()
+              );
+              $Technical->setTechnicalInventoryQuantityAvailable(
+                $readTechnical->technical_inventory_quantity_available + (int) request->technical_inventory_amount
+            );
+            $this->technicalInventoryModel->updateIncreaseTechnicalInventory($Technical);
+          }
+
+      }
+
+
+
+
+
+
+
+
+
+
+  //        $readTechnical=$this->technicalInventoryModel->readByTechnicalInventoryDB($Technical);
+  //        if(isset($readTechnical->idusers)) {
+  //           $Technical->setIdtechnicalInventory($readTechnical->idtechnical_inventory);
+  //           $Technical->setTechnicalInventoryAmount(
+  //               $readTechnical->technical_inventory_amount + $Technical->getTechnicalInventoryAmount()
+  //           );
+  //           $Technical->setTechnicalInventoryQuantityAvailable(
+  //               $readTechnical->technical_inventory_quantity_available + (int) request->technical_inventory_amount
+  //           );
+  //           $this->technicalInventoryModel->updateIncreaseTechnicalInventory($Technical);
+  //           return response->success("La cantidad de repuestos que ´posees  se aumento ");
+  //       }
+  //       else{
+  //        $this->technicalInventoryModel->createTechnicalInventoryDB(
+  //           TechnicalInventory::formFields()
+  //           ->setIdserviceStates(6)
+  //           ->setTechnicalInventoryQuantityUsed(0)
+  //           ->setTechnicalInventoryQuantityAvailable((int) request->technical_inventory_amount)
+  //       );
+  //        return response->success("se Agrego una nueva solicitud de repuesto a tu inventario");
+
+  //    }
+  //    return $readSpareParts= $this->sparePartsModel->readBySparePartsDB($sparts->getIdspareParts());
+  //    $sparts->setSparePartsAmount($readSpareParts->spare_parts_amount);
+  //    if($sparts->getSparePartsAmount() >= $Technical->getTechnicalInventoryAmount()) {
+  //       $sparts->setSparePartsAmount($sparts->getSparePartsAmount() - $Technical->getTechnicalInventoryAmount());
+  //       $this->sparePartsModel->update_spare_parts_amount($sparts);
+  //       $responseUpdate = $this->technicalInventoryModel->updateTechnicalInventoryDB($Technical);
+  //       return response->success("Inventario del tecnico actualizado correctamente 3");
+  //   }
+  //   else{
+  //     return response->error("la cantidad ingresada sobre pasa a la que hay en el inventario");
+  // }
+// }
+     //      elseif ($Technical->getIdserviceStates() == 8) {
+     //         if( $Technical->getTechnicalInventoryQuantityUsed() <= $Technical->getTechnicalInventoryAmount()) {
+     //            $Technical->setTechnicalInventoryAmount(
+     //                $Technical->getTechnicalInventoryAmount() - $Technical->getTechnicalInventoryQuantityUsed()
+     //            );
+     //            return $Technical;
+     //    // $responseUpdate = $this->technicalInventoryModel->updateReduceTechnicalInventoryDB($Technical);
+     //    // return response->success("Inventario del tecnico actualizado correctamente 8");
+     //        }
+     //        else{
+     //         return response->error("Error la cantidad de ingresada de repuestos usados es mayor a la cantidad de repuesto que tiene en su inventario verifique porfavor");
+     //     }
+     // }
+  }
 
 }
--- app/Models/Service/TechnicalInventoryModel.php
+++ app/Models/Service/TechnicalInventoryModel.php
@@ -30,11 +30,19 @@ class TechnicalInventoryModel {
         ])->execute();
     }
 
-     public function updateReduceTechnicalInventoryDB(TechnicalInventory $technicalInventory) {
+    public function updateReduceTechnicalInventoryDB(TechnicalInventory $technicalInventory) {
         return DB::call('update_reduce_technical_inventory', [
             $technicalInventory->getTechnicalInventoryAmount(),
             $technicalInventory->getTechnicalInventoryQuantityUsed(),
             $technicalInventory->getIdserviceStates(),
+            $technicalInventory->getIdtechnicalInventory()
+        ])->execute();
+    }
+    public function updateIncreaseTechnicalInventory(TechnicalInventory $technicalInventory) {
+        return DB::call('update_increase_technical_inventory', [
+            $technicalInventory->getTechnicalInventoryAmount(),
+            $technicalInventory->getIdserviceStates(),
+            $technicalInventory->getTechnicalInventoryQuantityAvailable(),
             $technicalInventory->getIdtechnicalInventory()
         ])->execute();
     }
@@ -43,6 +51,14 @@ class TechnicalInventoryModel {
         return DB::table('read_technical_inventory')
         ->select()
         ->getAll();
+    }
+
+    public function readByTechnicalInventoryDB(TechnicalInventory $technicalInventory) {
+        return DB::table('technical_inventory')
+        ->select()
+        ->where(DB::equalTo('idusers'),$technicalInventory->getIdusers())
+        ->and(DB::equalTo('idspare_parts'),$technicalInventory->getIdspareParts())
+        ->get();
     }
 
 }
