ec457315057ef114acebbe94e341aaea4a6ccc68
Sergio León <sergioleon4004@hotmail.com>
2023-01-20 12:41:19 -0500

    feat: entities have been created for payments

---
+++ app/Http/Controllers/Manage/PaymentsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Manage;
+
+class PaymentsController {
+
+	public function __construct() {
+
+	}
+
+}
---
+++ app/Models/Manage/PaymentsModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Manage;
+
+use LionSQL\Drivers\MySQL as DB;
+
+class PaymentsModel {
+
+	public function __construct() {
+		
+	}
+
+}
