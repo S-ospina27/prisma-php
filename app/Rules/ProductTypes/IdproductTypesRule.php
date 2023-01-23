96be9fae3a55538e33c38071ee5d4bcb7f733440
Sergio León <sergioleon4004@hotmail.com>
2023-01-23 12:36:58 -0500

    fix: rules have been corrected

--- app/Rules/ProductTypes/IdproductTypesRule.php
+++ app/Rules/ProductTypes/IdproductTypesRule.php
@@ -11,16 +11,16 @@ class IdproductTypesRule {
 	public static function passes(): void {
 		self::validate(function(\Valitron\Validator $validator) {
 			$validator
-			->rule("required", "idproduct_types")
-			->message("El tipo de producto es  requerido");
+    			->rule("required", "idproduct_types")
+    			->message("El tipo de producto es  requerido");
 
 			$validator
-			->rule("integer", "idproduct_types")
-			->message("El tipo de producto no es valido");
+    			->rule("integer", "idproduct_types")
+    			->message("El tipo de producto no es valido");
 
 			$validator
-			->rule("min", "idproduct_types", 1)
-			->message("El tipo de producto no es valido");
+    			->rule("min", "idproduct_types", 1)
+    			->message("El tipo de producto no es valido");
 		});
 	}
 
--- app/Rules/ProductTypes/ProductTypesNameRule.php
+++ app/Rules/ProductTypes/ProductTypesNameRule.php
@@ -11,25 +11,25 @@ class ProductTypesNameRule {
 	public static function passes(): void {
 		self::validate(function(\Valitron\Validator $validator) {
 			$validator
-			->addRule('name', function($field, $value, array $params, array $fields) {
-				return preg_match("/^([a-zA-Z ]+)(\s[a-zA-Z ]+)*$/", $value);
-			});
+    			->addRule('types_name', function($field, $value, array $params, array $fields) {
+    				return preg_match("/^([a-zA-Z0-9 ,-]+)(\s[a-zA-Z0-9 ,-]+)*$/", $value);
+    			});
 
 			$validator
-			->rule("required", "product_types_name")
-			->message("El nombre del tipo de producto es requerido");
+    			->rule("required", "product_types_name")
+    			->message("El tipo de producto es requerido");
 
 			$validator
-			->rule("lengthMin", "product_types_name", 2)
-			->message("El nombre del tipo de producto debe tener mínimo 2 caracteres");
+    			->rule("lengthMin", "product_types_name", 2)
+    			->message("El tipo de producto debe tener mínimo 2 caracteres");
 
 			$validator
-			->rule("lengthMax", "product_types_name", 25)
-			->message("El nombre del tipo de producto debe tener máximo 25 caracteres");
+    			->rule("lengthMax", "product_types_name", 25)
+    			->message("El tipo de producto debe tener máximo 25 caracteres");
 
 			$validator
-			->rule("name", "product_types_name")
-			->message("El nombre solo debe tener caracteres alfabeticos");
+    			->rule("types_name", "product_types_name")
+    			->message("El tipo de producto solo debe tener caracteres alfa-numéricos ('A-Z', '0-9', ',-')");
 		});
 	}
 
